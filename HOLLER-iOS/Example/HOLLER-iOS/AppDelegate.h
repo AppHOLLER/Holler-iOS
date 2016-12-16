27eaa5d62f74975115c6333e29fa2a6d2c31271f
KhuongPham <khuong.pham@rainmaker-labs.com>
2016-12-16 14:17:06 +0700

    Change HLError

---
+++ HOLLER-iOS/Example/HOLLER-iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HOLLER-iOS
+//
+//  Created by KhuongPham on 12/16/2016.
+//  Copyright (c) 2016 KhuongPham. All rights reserved.
+//
+
+@import UIKit;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HOLLER-iOS/Example/Pods/Target Support Files/Pods-HOLLER-iOS_Example/Pods-HOLLER-iOS_Example-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_HOLLER_iOS_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HOLLER_iOS_ExampleVersionString[];
+
