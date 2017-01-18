b1e0b1dda28fcf4343d6042dc10783c23833efe3
Huynh Nguyen <nguyenhuynh2688@gmail.com>
2017-01-18 11:30:14 +0700

    Integrated SDK to sample and added cert to git

--- HOLLER-iOS/Example/HOLLER-iOS/AppDelegate.m
+++ HOLLER-iOS/Example/HOLLER-iOS/AppDelegate.m
@@ -7,12 +7,25 @@
 //
 
 #import "AppDelegate.h"
+#import "../../HOLLER-iOS/Classes/Holler.h"
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    
+    
+    [Holler startHolleringWithApplicationId:@"8f605bcb726d0ce14c41c6f7452c60a7fe803a15" accessId:@"7dfd3bef93a7c09117f8745c7f771ef643d911a3"];
+    
+    UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
+                                                    UIUserNotificationTypeBadge |
+                                                    UIUserNotificationTypeSound);
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes categories:nil];
+    [application registerUserNotificationSettings:settings];
+    
     return YES;
 }
 
@@ -20,6 +33,7 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -31,16 +45,29 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [Holler applicationDidBecomeActive:application];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [Holler applicationWillTerminate:application];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    [Holler didRegisterForRemoteNotificationWithDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    [Holler didReceiveRemoteNotification:userInfo];
 }
 
 @end
--- HOLLER-iOS/Example/HOLLER-iOS/ViewController.m
+++ HOLLER-iOS/Example/HOLLER-iOS/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "../../HOLLER-iOS/Classes/HLSubscriber.h"
 
 @interface ViewController ()
 
@@ -19,12 +19,41 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+   
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)regristerPushNotification:(UIButton *)sender {
+    HLSubscriber *subscriber = [[HLSubscriber alloc] init];
+    [subscriber setSubscriberFirstName:@"rain"];
+    [subscriber setSubscriberLastName:@"maker"];
+    [subscriber setSubscriberEmail:@"nguyenhuynh2688@gmail.com"];
+    [subscriber setSubscriberUsername:@"rainMaker"];
+    [subscriber setSubscriberCellphoneNumber:@"841233744033"];
+    
+    [subscriber setSubscriberInformations:@{@"gps_latitude": @0,
+                                            @"gps_longitude": @0,
+                                            @"gender": @"male",
+                                            @"date_of_birth": @"2016/6/12",
+                                            @"country": @"SG",
+                                            @"industry": @"",
+                                            @"designation": @"",
+                                            @"interest_id": @""}];
+    [subscriber registerSubscriberOnCompletion:^(BOOL succeed, HLError *error, NSNumber *subscriberId) {
+        if (error) {
+            //handle error object with description and error status
+            NSLog(@"Error");
+        } else {
+            //succeed. subscriberId'll returned back
+            NSLog(@"Success");
+        }
+    }];
 }
 
 @end
